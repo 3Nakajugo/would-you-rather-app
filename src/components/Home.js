5230f8d70b63db6a16e3ab2555e2d5b9fd05f7f2
3Nakajugo <edinahmargarita@gmail>
2021-09-27 00:21:11 +0300

    feat (home): add home commopent
    add home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,52 @@
-import React, { Component } from 'react'
-import Question from './Question'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import Question from "./Question";
+import { connect } from "react-redux";
+import { Tab, Tabs } from "react-bootstrap";
 
 class Home extends Component {
   render() {
-    const { answered, unanswered } = this.props
+    const { answered, unanswered } = this.props;
     return (
-
-      // <ul className="nav nav-pills nav-justified">
-      //   <li className="active"><a href="#">Unanswered questions</a></li>
-      //   <li><a href="#"> Answeed questions</a></li>
-      // </ul>
-      <div className="align-self-center">
-        <div>
-          {unanswered.map(id => (
-            <Question key={id} id={id} />
-          ))}
-        </div>
-        <div>
-          {answered.map(id => (
-            <Question key={id} id={id} />
-          ))}
-        </div>
-
+      <div className="questions-container">
+        <Tabs
+        fill justify
+          defaultActiveKey="unanswered"
+          transition={false}
+          id="uncontrolled-tab-example"
+          ClassName="tabs"
+        >
+          <Tab
+            eventKey="unanswered"
+            title="Unanswered Questions"
+            align="center"
+            width="auto"
+          >
+            {unanswered.map((id) => (
+              <Question id={id} key={id} />
+            ))}
+          </Tab>
+          <Tab
+            eventKey="answered"
+            title="Answered Questions"
+            align="center"
+          >
+            {answered.map((id) => (
+              <Question id={id} key={id} />
+            ))}
+          </Tab>
+        </Tabs>
       </div>
-    )
+    );
   }
 }
 
-
 function mapStateToProps({ questions, authedUser }) {
-
-  let loggedInUser = authedUser["authedUser"]
+  let loggedInUser = authedUser["authedUser"];
 
   return {
     questions,
     answered: Object.keys(questions)
-      .filter(id => {
+      .filter((id) => {
         return (
           questions[id].optionOne.votes.includes(loggedInUser) ||
           questions[id].optionTwo.votes.includes(loggedInUser)
@@ -45,14 +55,14 @@ function mapStateToProps({ questions, authedUser }) {
       .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
 
     unanswered: Object.keys(questions)
-      .filter(id => {
+      .filter((id) => {
         return (
           !questions[id].optionOne.votes.includes(loggedInUser) &&
           !questions[id].optionTwo.votes.includes(loggedInUser)
         );
       })
-      .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
-  }
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+  };
 }
 
-export default connect(mapStateToProps)(Home)
+export default connect(mapStateToProps)(Home);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,62 +1,75 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getAuthedUser } from '../actions/authedUser';
-import { withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getAuthedUser } from "../actions/authedUser";
+import { withRouter } from "react-router-dom";
 
 export class Login extends Component {
-
-  state = { user: '' };
-
+  state = { user: "" };
 
   handleChange = (e) => {
     e.preventDefault();
     const value = e.target.value;
     this.setState({ user: value });
-  }
+  };
 
   handleSubmit = (e) => {
-    const { dispatch, history } = this.props
+    const { dispatch, history } = this.props;
     e.preventDefault();
-    if (this.state.user === '') {
-      alert('failed to login ')
+    if (this.state.user === "") {
+      alert("failed to login ");
     } else {
       dispatch(getAuthedUser(this.state.user));
       history.push("/");
     }
-
-  }
+  };
   render() {
-    const { users } = this.props
+    const { users } = this.props;
     return (
-
-      <div className="login-container card Regular shadow" >
+      <div className="login-container card Regular shadow">
         <div className="card-header">
           <h2> Welcome To The Would You Rather App!</h2>
           <p>Please sign in to continue</p>
         </div>
         <div className=" login-content">
           <h5> Sign In</h5>
-          <select value={this.state.user} onChange={this.handleChange} className="form-select form-select-lg mb-3" aria-label=".form-select-lg example" >
-            <option value="Select a user..." >Select a user...</option>
+          <select
+            value={this.state.user}
+            onChange={this.handleChange}
+            className="form-select form-select-lg mb-3"
+            aria-label=".form-select-lg example"
+          >
+            <option value="Select a user...">Select a user...</option>
             {Object.keys(users).map((user) => (
               <option key={users[user].id} value={users[user].id}>
                 {users[user].name}
               </option>
             ))}
           </select>
-          <button onClick={this.handleSubmit} type="submit" className="btn shadow-none" style={{ backgroundColor: "#09b1a8", color: "#ffffff", width: '100%', marginBottom: '15px', marginTop: '20px' }}>Sign In</button>
+          <button
+            onClick={this.handleSubmit}
+            type="submit"
+            className="btn shadow-none"
+            style={{
+              backgroundColor: "#09b1a8",
+              color: "#ffffff",
+              width: "100%",
+              marginBottom: "15px",
+              marginTop: "20px",
+            }}
+          >
+            Sign In
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
 function mapStateToProps({ users, authedUser }) {
   return {
-    isLoggedIn: authedUser !== '' ? true : false,
-    users
-  }
-
+    isLoggedIn: authedUser !== "" ? true : false,
+    users,
+  };
 }
 
-export default withRouter(connect(mapStateToProps)(Login))
+export default withRouter(connect(mapStateToProps)(Login));
